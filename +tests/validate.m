b8848f3bb9577cfdf2c1f508203f54932e05db66
Naor Movshovitz <nmovshov@gmail.com>
2022-05-20 11:04:36 -0700

    skeleton for validate

--- +tests/validate.m
+++ +tests/validate.m
@@ -1,13 +1,21 @@
-function tf = uniform()
-% UNIFORM Test k2 with uniform density.
+function T = validate(N)
+% VALIDATE Test k2 calculation against known cases.
 
-N = 100;
+M = [];
 zvec = linspace(1/N, 1, N);
+
+% Uniform density
 dvec = ones(size(zvec));
-k2 = lovek2(zvec,dvec);
+k2_calc = lovek2(zvec,dvec);
+k2_expect = 1.5;
+k2_err = abs(k2_calc - k2_expect)/abs(k2_expect + eps);
+row = [k2_expect, k2_calc, k2_err];
+M = [M; row];
 
-T = table(N,k2);
-disp(T)
 
-tf = true;
+% Tabelize
+T = table();
+T.expected = M(:,1);
+T.calculated = M(:,2);
+T.rel_err = M(:,3);
 end
